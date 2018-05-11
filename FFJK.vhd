071750f97a754f019514e07aede46378e9557d62
Danielly Costa <daniellycmcosta@gmail.com>
2018-05-11 16:23:45 -0300

    Corrigindo contagem

--- FFJK.vhd
+++ FFJK.vhd
@@ -13,13 +13,13 @@ architecture FFJK of FFJK is
 		begin
 			if (preset = '1') then estado <= '1';
 			elsif (clear = '1') then estado <='0';
-			elsif (clock='1' and clock 'event) then
+			elsif (clock='0' and clock 'event) then
 				if (j='0' and k='1') then estado <= '1';
 				elsif (j='1' and k='0') then estado <='0';
 				elsif (j='0' and k='0') then estado <= not estado;
 				end if;
 			end if;
 		end process;
-		q <= not estado;
+		q <= estado;
 	end FFJK;
 	
